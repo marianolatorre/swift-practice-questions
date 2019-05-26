f8cd9e04721c3a33a942df32e288beeaa31f2efc
mariano latorre <marianolatorre@gmail.com>
2019-05-26 02:06:04 +0100

    Avoid circular linked list infinite loops

--- print-linked-list.playground/Contents.swift
+++ print-linked-list.playground/Contents.swift
@@ -2,12 +2,18 @@
 Printed linked list values
  */
 
-class Node<T> {
+class Node<T>: Equatable {
+
+
     var value: T
     var next: Node?
 
     init(value: T) {
         self.value = value
+    }
+
+    static func == (lhs: Node<T>, rhs: Node<T>) -> Bool {
+        return lhs === rhs
     }
 }
 
@@ -19,13 +25,23 @@ var linkedListC = Node(value: 4)
 linkedList.next = linkedListA
 linkedListA.next = linkedListB
 linkedListB.next = linkedListC
+linkedListC.next = linkedListA
+
+var visited: [Node<Int>] = [Node<Int>]()
 
 func printList(_ root: Node<Int>) -> String {
 
     var result = String(root.value)
+    
+    // cache visited items
+    visited.append(root)
+
     if let next = root.next {
-        result.append(contentsOf: ", ")
-        result.append(contentsOf: printList(next))
+        // Let's avoid printing infinite loops in the linked list!
+        if visited.contains(next) == false {
+            result.append(contentsOf: ", ")
+            result.append(contentsOf: printList(next))
+        }
     }
 
     return result
