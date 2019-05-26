9467d07dda34697368d80e9f72f5c75b09dc9320
mariano latorre <marianolatorre@gmail.com>
2019-05-26 01:29:35 +0100

    pair words that are anagrams to each other from an array of words assuming no duplicates in array for simplicity

--- reverse-string.playground/Contents.swift
+++ reverse-string.playground/Contents.swift
@@ -28,3 +28,34 @@ for input in scenarios {
 for input in scenarios {
     print(reverseLoop(input))
 }
+
+/*
+pair words that are anagrams to each other from an array of words
+ assuming no duplicates in array for simplicity
+ */
+
+let candidates = ["hello","bye","olleh","aeyb","eyb"]
+
+func findAnagramPairs(_ input: [String]) -> [(String,String)] {
+
+    let invertedWords = candidates.compactMap{ reverse($0) }
+    var results = [(String, String)]()
+
+    var posX = 0
+    var posY = 1
+
+    while posX < candidates.count - 1 {
+        posY = posX + 1
+        while posY < candidates.count {
+            if candidates[posX] == invertedWords[posY] {
+                results.append((candidates[posX], candidates[posY]))
+            }
+            posY += 1
+        }
+        posX += 1
+    }
+
+    return results
+}
+
+print(findAnagramPairs(candidates))  //[("hello", "olleh"), ("bye", "eyb")]
